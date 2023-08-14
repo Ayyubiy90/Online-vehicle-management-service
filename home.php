c2b10c136f6e665b9d2281f170df39c035cc4692
ayb <ayyubiy67@gmail.com>
2023-08-14 14:34:29 +0100

    pushing my project to github

---
+++ home.php
@@ -0,0 +1,58 @@
+ <!-- Header-->
+ <header class="bg-dark py-5" id="main-header">
+    <div class="container h-100 d-flex align-items-end justify-content-center w-100">
+        <div class="text-center text-white w-100">
+            <h1 class="display-4 fw-bolder mx-5"></h1>
+            <div class="col-auto mt-2">
+            <h1 class="display-4 fw-bolder">Vehicle Service Management System</h1>
+                <br/>
+                <a class="btn btn-primary btn-lg rounded-0" href="./?p=services">Service Request Now</a>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- Section-->
+
+<script>
+    $(function(){
+        $('#search').on('input',function(){
+            var _search = $(this).val().toLowerCase().trim()
+            $('#service_list .item').each(function(){
+                var _text = $(this).text().toLowerCase().trim()
+                    _text = _text.replace(/\s+/g,' ')
+                    console.log(_text)
+                if((_text).includes(_search) == true){
+                    $(this).toggle(true)
+                }else{
+                    $(this).toggle(false)
+                }
+            })
+            if( $('#service_list .item:visible').length > 0){
+                $('#noResult').hide('slow')
+            }else{
+                $('#noResult').show('slow')
+            }
+        })
+        $('#service_list .item').hover(function(){
+            $(this).find('.callout').addClass('shadow')
+        })
+        $('#service_list .view_service').click(function(){
+            uni_modal("Service Details","view_service.php?id="+$(this).attr('data-id'),'mid-large')
+        })
+        $('#send_request').click(function(){
+            uni_modal("Fill the Service Request Form","send_request.php",'large')
+        })
+
+    })
+    $(document).scroll(function() { 
+        $('#topNavBar').removeClass('bg-transparent navbar-dark bg-primary')
+        if($(window).scrollTop() === 0) {
+           $('#topNavBar').addClass('navbar-dark bg-transparent')
+        }else{
+           $('#topNavBar').addClass('navbar-dark bg-primary')
+        }
+    });
+    $(function(){
+        $(document).trigger('scroll')
+    })
+</script>
